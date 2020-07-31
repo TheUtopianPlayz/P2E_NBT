b3de2657cc1742f84798c3c4ab7214e34f417540
matthewmenser@gamil.com <a19982809>
2020-07-31 07:51:34 -0500

    could have sworn i pushed the code

--- src/TheUtopian/p2e_nbt/P2E_NBT.php
+++ src/TheUtopian/p2e_nbt/P2E_NBT.php
@@ -3,7 +3,25 @@ declare(strict_types=1);
 
 namespace TheUtopian\p2e_nbt;
 
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-class P2E_NBT extends PluginBase{
+
+class P2E_NBT extends PluginBase
+{
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    {
+        switch ($command->getName()) {
+            case "nbt":
+                if (!$sender instanceof Player) {
+                    $sender->sendMessage("§cThis command can be used only in game!");
+                    return true;
+                }
+                $nbt = $sender->getInventory()->getItemInHand()->getNamedTag()->toString();
+                $this->getServer()->broadcastMessage("§l§c$nbt");
+        }
+        return true;
+    }
 
 }
